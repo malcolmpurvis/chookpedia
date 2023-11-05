1175d1074bb42a5da521c8e81dbd28702f5173bb
Malcolm Purvis <malcolm@purvis.id.au>
2023-11-05 16:42:02 +1100

    Initial file from the Spring Initializer.

---
+++ src/main/kotlin/au/id/purvis/chookpedia/ChookpediaApplication.kt
@@ -0,0 +1,11 @@
+package au.id.purvis.chookpedia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChookpediaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChookpediaApplication>(*args)
+}
---
+++ src/test/kotlin/au/id/purvis/chookpedia/ChookpediaApplicationTests.kt
@@ -0,0 +1,13 @@
+package au.id.purvis.chookpedia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChookpediaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
